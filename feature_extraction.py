b66cdd8775137b59bd50f6510feddcc484818d40
boxmein <johannes@qminderapp.com>
2017-03-18 19:25:38 +0200

    Add code for feature extraction with VGG

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,7 @@
 import pickle
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import *
 # TODO: import Keras layers you need here
 
 flags = tf.app.flags
@@ -41,13 +43,26 @@ def main(_):
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
 
+    classes = len(np.unique(y_train))
+
+    model = Sequential()
+    model.add(Flatten(input_shape=X_train.shape[1:]))
+    model.add(Dense(classes, activation='softmax'))
+
     # TODO: define your model and hyperparams here
     # make sure to adjust the number of classes based on
     # the dataset
     # 10 for cifar10
     # 43 for traffic
 
+    model.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
+
     # TODO: train your model here
+
+    model.fit(X_train, y_train, epochs=50, batch_size=512,
+                validation_data=(X_val, y_val), shuffle=True)
 
 
 # parses flags and calls the `main` function above
